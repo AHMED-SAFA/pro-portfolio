86f6283ab460b397677b320721191fd92075f43d
safa <ahmedsafa0759@gmail.com>
2024-07-31 20:54:26 +0600

    counter added, project added

---
+++ assets/js/counter.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  let visitorCount = localStorage.getItem("visitorCount");
+
+  if (!visitorCount) {
+    visitorCount = 0;
+  } else {
+    visitorCount = parseInt(visitorCount);
+  }
+
+  visitorCount += 1;
+  localStorage.setItem("visitorCount", visitorCount);
+
+  document.getElementById("visitor-count").innerText = visitorCount;
+//   localStorage.removeItem("visitorCount");
+});
